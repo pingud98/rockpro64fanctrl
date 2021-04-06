3cce214343a47d7ce91a710cf8beedfcd48224fa
James Devine <devine.jd@gmail.com>
2021-04-06 23:17:11 +0200

    had a header. removed.

--- fancontrol.sh
+++ fancontrol.sh
@@ -1,3 +1,2 @@
-  GNU nano 4.8                                                            fancontrol.sh                                                                      
 #!/bin/bash
 echo "20" > /sys/class/hwmon/hwmon3/pwm1 
